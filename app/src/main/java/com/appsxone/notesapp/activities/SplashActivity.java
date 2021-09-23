aa6380f71a85df9a31b507f4298892eb282780f5
Asad Rao <raoasad125@gmail.com>
2021-09-23 21:58:28 +0500

    Till, Trash Feature.

---
+++ app/src/main/java/com/appsxone/notesapp/activities/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.appsxone.notesapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.appsxone.notesapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
