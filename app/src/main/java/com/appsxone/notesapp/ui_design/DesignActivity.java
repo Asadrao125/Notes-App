bd5d79310aac98a3fdaddb183c9c39eae271e399
Asad Rao <raoasad125@gmail.com>
2021-09-29 14:15:17 +0500

    Till, Added 3 time Alarm Function with Random Quote (Morning, Evening and Night).

--- app/src/main/java/com/appsxone/notesapp/ui_design/DesignActivity.java
+++ app/src/main/java/com/appsxone/notesapp/ui_design/DesignActivity.java
@@ -1,16 +1,99 @@
 package com.appsxone.notesapp.ui_design;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.appsxone.notesapp.R;
 
 public class DesignActivity extends AppCompatActivity {
+    CardView cv1, cv2, cv3, cv4, cv5, cv6, cv7, cv8, cv9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design);
+
+        cv1 = findViewById(R.id.cv1);
+        cv2 = findViewById(R.id.cv2);
+        cv3 = findViewById(R.id.cv3);
+        cv4 = findViewById(R.id.cv4);
+        cv5 = findViewById(R.id.cv5);
+        cv6 = findViewById(R.id.cv6);
+        cv7 = findViewById(R.id.cv7);
+        cv8 = findViewById(R.id.cv8);
+        cv9 = findViewById(R.id.cv9);
+
+        cv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cv9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
